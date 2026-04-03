688e0d0d66159cb99cb7c37f98e2eade7d6de28a
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2026-04-03 09:40:31 -0400

    Update baseUrl to my-system

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "cdawgfromtheancientlands.github.io/my-system",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
